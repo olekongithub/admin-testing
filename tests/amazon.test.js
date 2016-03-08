407072013b1176d89aa377890cd004cb2e8ec887
Olek <malahoff@gmail.com>
2016-03-07 19:45:32 -0500

    amazon simplified, less pausing

--- tests/amazon.test.js
+++ tests/amazon.test.js
@@ -7,44 +7,46 @@ const common = require('./common'),
 let itemName;
 
 describe.only('shop for shoes on amazon', () => {
-  it('should load the page', () => {
-    return browser.url('/').getTitle()
+  it('should load the page', () => 
+    browser.url('/').getTitle()
       .then(title => expect(title).to.startWith('Amazon.com'))
-  });
+  )
 
-  it('should search for blue suede shoes', () => {
-  	return browser.setValue('//input[@id="twotabsearchtextbox"]', 'blue suede shoes') 
-  	  .click('//input[@value="Go"]');
-  });
+  it('should search for blue suede shoes', () => 
+  	browser.setValue('//input[@id="twotabsearchtextbox"]', 'blue suede shoes') 
+  	  .click('//input[@value="Go"]')
+  )
   
-  it('should find the search page', () => {
-  	return browser.getTitle()
+  it('should find the search page', () =>
+  	browser.getTitle()
   	  .then(title => expect(title).to.startWith('Amazon.com: blue suede shoes'))
-  })
+  )
 
-  it('should choose the first option', () => {
-  	return browser.pause(1000)
+  it('should choose the first option', () =>
+  	browser.waitForVisible('.s-access-detail-page')
   	  .click('.s-access-detail-page')
-  })
+  )
 
-  it('should be the first item', () => {
-  	return browser.getText('#productTitle')
- 	  .then(title => itemName = title)
-  })
+  it('should be the first item', () =>
+  	browser.waitForVisible('#productTitle')
+      .getText('#productTitle')
+ 	    .then(title => itemName = title)
+  )
 
-  it('should order the shoes', () => {
-  	return browser.selectByIndex('#native_dropdown_selected_size_name', 3)
-  	  .pause(1000)
+  it('should order the shoes', () =>
+  	browser.waitForVisible('#native_dropdown_selected_size_name')
+      .selectByIndex('#native_dropdown_selected_size_name', 3)
+  	  .pause(500)
   	  .click('#add-to-cart-button')
-  	  .pause(1000)
-  	  .click('#nav-cart')
-  })
+  	  .waitForVisible('#hlb-view-cart-announce')
+  	  .click('#hlb-view-cart-announce')
+  )
 
-  it('should be in the cart', () => {
-  	return browser.pause(2000)
+  it('should be in the cart', () =>
+  	browser.waitForVisible('div.sc-item-product-image > .sc-product-link')
   	  .click('div.sc-item-product-image > .sc-product-link')
   	  .getText('#productTitle')
   	  .then(text => expect(text).to.equal(itemName))
-  })
+  )
 
 });
